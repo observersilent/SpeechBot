1cb9c3756f636135ea3cf93b2be16a12cf6dde60
observersilent <denek888@mail.ru>
2022-08-29 14:36:39 +0500

    Обновлен main.py

--- main.py
+++ main.py
@@ -7,14 +7,20 @@ from telegram.ext import (Application, CommandHandler, ContextTypes,
 from typing import Dict
 
 from multiprocessing import Process
-#from G2P import G2P
 from Database import Database
+from TelegramAPI import TelegramAPI
 
 # Включение логирования
 logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
 )
 logger = logging.getLogger(__name__)
+
+# База данных
+database = Database()
+# Элемент API
+api = TelegramAPI()
+#---------------------------------------------------------------------------------------------------
 
 # Стадии опроса пользователя
 CHOOSING_OPER, REPORT_REPLY, SPEACH_FORMAT, SPEACH_SPEAKER, SPEACH_TEXT, SPEACH_REPLY = range(6)
@@ -33,33 +39,17 @@ reply_speach_keyboard_commit = [
 ]
 markup_speach_commit = ReplyKeyboardMarkup(reply_speach_keyboard_commit, one_time_keyboard=True)
 
-reply_oper_keyboard = [
-    ["Генерация речи"],
-    ["Запрос статистики бота"],
-    ["Отмена"],
-]
+reply_oper_keyboard = [[type] for type in list(database.getTypeQueryDict().keys())]
+reply_oper_keyboard.append(["Отмена"])
 markup_oper = ReplyKeyboardMarkup(reply_oper_keyboard, one_time_keyboard=True)
 
-reply_format_keyboard = [
-    ["WAV"],
-    ["OGG"],
-    ["MP3"],
-    ["Отмена"]
-]
+reply_format_keyboard = [[format] for format in list(database.getFormatDict().keys())]
+reply_format_keyboard.append(["Отмена"])
 markup_format = ReplyKeyboardMarkup(reply_format_keyboard, one_time_keyboard=True)
 
-reply_speaker_keyboard = [
-    ["RUSLAN"],
-    ["Артемий Лебедев"],
-    ["Отмена"]
-]
+reply_speaker_keyboard = [[speaker] for speaker in list(database.getSpeakerDict().keys())]
+reply_speaker_keyboard.append(["Отмена"])
 markup_speaker = ReplyKeyboardMarkup(reply_speaker_keyboard, one_time_keyboard=True)
-
-reply_speach_keyboard_commit = [
-    ["Сгенерировать речь"],
-    ["Отмена"],
-]
-markup_speach_commit = ReplyKeyboardMarkup(reply_speach_keyboard_commit , one_time_keyboard=True)
 # ---------------------------------------------------------------------------------------------------
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
@@ -73,7 +63,7 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
 
 #Выбран отчёт
 async def report_choice(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
-    print('report_choice')
+    #print('report_choice')
     text = update.message.text
     context.user_data["type_oper"] = text
     await update.message.reply_text(f"Выбран тип операции \"{text}\".", reply_markup=markup_report_commit)
@@ -82,18 +72,32 @@ async def report_choice(update: Update, context: ContextTypes.DEFAULT_TYPE) -> i
 
 #Подтверждено что необходимо выполнить отчёт
 async def report_commit(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
-    print('report_commit')
+    #print('report_commit')
 
-    await update.message.reply_text(f"Команда подтверждена.\n"
-                                    f"Операция: {context.user_data['type_oper']}\n",
-                                    reply_markup=ReplyKeyboardRemove())
+    user_id = update.effective_user.id
+    type_query = context.user_data['type_oper']
+
+    id_chat = update.message.chat.id
+    id_user_role = database.getUserRoleId(user_id)
+    id_type_query = database.getTypeQueryId(type_query)
+
+    errDatabase = database.insertQueryReport(id_chat, id_user_role, id_type_query)
+
+    if errDatabase is None:
+        await update.message.reply_text(f"Команда подтверждена.\n"
+                                        f"Операция: {type_query}\n",
+                                        reply_markup=ReplyKeyboardRemove())
+    else:
+        await update.message.reply_text(f"Возникла непредвиденная ошибка. Попробуйте позже. \n",
+                                        reply_markup=ReplyKeyboardRemove())
+
 
     context.user_data.clear()
     return ConversationHandler.END
 
 #Выбрана генерация речи
 async def speach_choice(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
-    print('speach_choice')
+    #print('speach_choice')
     text = update.message.text
     context.user_data["type_oper"] = text
     await update.message.reply_text(f"Выбран тип операции \"{text}\".", reply_markup=markup_format)
@@ -102,7 +106,7 @@ async def speach_choice(update: Update, context: ContextTypes.DEFAULT_TYPE) -> i
 
 #Выбран выходной формат речи
 async def speach_format_choice(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
-    print('speach_format_choice')
+    #print('speach_format_choice')
     text = update.message.text
     context.user_data["type_format"] = text
     await update.message.reply_text(f"Выбран формат выходного файла \"{text}\".", reply_markup=markup_speaker)
@@ -111,7 +115,7 @@ async def speach_format_choice(update: Update, context: ContextTypes.DEFAULT_TYP
 
 # Выбран спикер
 async def speach_speaker_choice(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
-    print('speach_speaker_choice')
+    #print('speach_speaker_choice')
     text = update.message.text
     context.user_data["type_speaker"] = text
     await update.message.reply_text(f"Выбран спикер \"{text}\". \n\n"
@@ -121,7 +125,7 @@ async def speach_speaker_choice(update: Update, context: ContextTypes.DEFAULT_TY
 
 # Выбран текст для озвучки
 async def speach_text_choice(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
-    print('speach_text_choice')
+    #print('speach_text_choice')
     text = update.message.text
     context.user_data["speach_text"] = text
     await update.message.reply_text(f"Текст который необходимо озвучить: \"{text}\".", reply_markup=markup_speach_commit)
@@ -129,20 +133,39 @@ async def speach_text_choice(update: Update, context: ContextTypes.DEFAULT_TYPE)
     return SPEACH_REPLY
 
 async def speach_commit(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
-    print('speach_commit')
+    #print('speach_commit')
 
-    await update.message.reply_text(f"Команда подтверждена.\n"
-                                    f"Операция: {context.user_data['type_oper']}\n"
-                                    f"Выходной формат файла: {context.user_data['type_format']}\n"
-                                    f"Спикер озвучки: {context.user_data['type_speaker']}\n\n"
-                                    f"Текст для озвучки: {context.user_data['speach_text']}",
-                                    reply_markup=ReplyKeyboardRemove())
+    user_id = update.effective_user.id
+    type_query = context.user_data['type_oper']
+    format = context.user_data['type_format']
+    speaker = context.user_data['type_speaker']
+    text = context.user_data['speach_text']
+
+    id_chat = update.message.chat.id
+    id_user_role = database.getUserRoleId(user_id)
+    id_type_query = database.getTypeQueryId(type_query)
+    id_format = database.getFormatId(format)
+    id_speaker = database.getSpeakerId(speaker)
+
+    errDatabase = database.insertQuerySpeach(id_chat, id_user_role, id_type_query, id_format, id_speaker, text)
+
+    if errDatabase is None:
+        await update.message.reply_text(f"Команда подтверждена.\n"
+                                        f"Операция: {type_query}\n"
+                                        f"Выходной формат файла: {format}\n"
+                                        f"Спикер озвучки: {speaker}\n\n"
+                                        f"Текст для озвучки: {text}",
+                                        reply_markup=ReplyKeyboardRemove())
+    else:
+        await update.message.reply_text(f"Возникла непредвиденная ошибка. Попробуйте позже. \n",
+                                        reply_markup=ReplyKeyboardRemove())
+
     context.user_data.clear()
     return ConversationHandler.END
 
 #Отмена
 async def done(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
-    print('вызвался done')
+    #print('вызвался done')
 
     await update.message.reply_text(
         f"Запрос отменен. \nДля новой попытки введите /start",
@@ -167,20 +190,34 @@ async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     #kek = transcriptor.getTranscript(update.message.text)
     #await update.message.reply_text(str(kek))
 
-def testFunction(g):
-    database = Database()
+def processQuery(g):
+    from G2P import G2P
+    transcriptor = G2P()
 
     while True:
-        print(g)
+        #print(g)
+        # Запросы на генерацию речи
+        listQuerySpeach = database.getUndoneQuerySpeach()
+        listError = []
+        for query in listQuerySpeach:
+            text = query[4]
+            textTranscript = transcriptor.getTranscript(text)
+            if textTranscript.startswith('Не известное слово'):
+                listError.append(('Генерация речи', query, textTranscript))
+                continue
+            print(textTranscript)
+        print(listError)
+        database.processError(listError, api)
+
         time.sleep(20)
 
 def main() -> None:
     # Запуск фонового процесса генерации речи
-    bgProcGenSpeech = Process(target=testFunction, kwargs={"g": "kek"})
+    bgProcGenSpeech = Process(target=processQuery, kwargs={"g": "kek"})
     bgProcGenSpeech.start()
 
     # Запуск опроса пользователей для сохранения списка запросов в БД
-    application = Application.builder().token("5446887600:AAH4OwDd9GfeD0Zl7mpiQso8EI2bsREJuCY").build()
+    application = Application.builder().token(TelegramAPI._token).build()
 
     conv_handler = ConversationHandler(
         entry_points=[CommandHandler("start", start)],
